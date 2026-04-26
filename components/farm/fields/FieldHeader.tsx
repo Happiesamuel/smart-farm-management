5b3dc5016d69a26adc0775f118aa390841738c17
Happiesamuel <odionsamuel2005@gmail.com>
2026-04-26 02:17:16 +0100

    correct field header

--- components/farm/fields/FieldHeader.tsx
+++ components/farm/fields/FieldHeader.tsx
@@ -4,13 +4,13 @@ import { IoSearch } from "react-icons/io5";
 
 export default function FieldHeader() {
   return (
-    <div className="flex flex-col gap-2 sm:flex-row items-center justify-between">
+    <div className="flex gap-2 items-center justify-between">
       <h6 className="text-dark text-base font-semibold">3 Fields</h6>
 
-      <div className="flex w-full sm:w-[35%] items-center border border-border gap-2 rounded-lg px-2">
+      <div className="flex w-[65%] sm:w-[45%] items-center border border-border gap-2 rounded-lg px-2">
         <IoSearch />
         <Input
-          placeholder="Search Farms..."
+          placeholder="Search Fields..."
           className="border-none p-0 group focus-visible:none shadow-none"
         />
       </div>
