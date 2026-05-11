626dcd99a60a6fd606c99930407d3005b66788f2
Happiesamuel <odionsamuel2005@gmail.com>
2026-05-11 16:46:54 +0100

    added suspense

--- app/(worker)/worker/tasks/page.tsx
+++ app/(worker)/worker/tasks/page.tsx
@@ -3,16 +3,18 @@ import TaskGroup from "@/components/worker/task/TaskGroup";
 import TaskHeader from "@/components/worker/task/TaskHeader";
 import TaskList from "@/components/worker/task/TaskList";
 import { useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 
 export default function Page() {
   const searchParams = useSearchParams();
   const [fil, setFil] = useState(searchParams.get("filter") || "all");
   return (
-    <div className="pt-18 px-2 sm:px-4 pb-8">
-      <TaskHeader />
-      <TaskGroup active={fil} setActive={setFil} />
-      <TaskList filter={fil} />
-    </div>
+    <Suspense fallback={<p>Loading...</p>}>
+      <div className="pt-18 px-2 sm:px-4 pb-8">
+        <TaskHeader />
+        <TaskGroup active={fil} setActive={setFil} />
+        <TaskList filter={fil} />
+      </div>
+    </Suspense>
   );
 }
