8ba7327080396eea1f361d4ffd3515dc277f8cb0
Happiesamuel <odionsamuel2005@gmail.com>
2026-05-17 10:29:15 +0100

    added modals to sales and expense

--- components/salesExpense/BothHeader.tsx
+++ components/salesExpense/BothHeader.tsx
@@ -1,8 +1,15 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import { FiDownload } from "react-icons/fi";
+import { useState } from "react";
+import { FiDownload, FiShoppingCart } from "react-icons/fi";
 import { GoPlus } from "react-icons/go";
+import { FinanceModal } from "../modals/FinanceModal";
+import { GrMoney } from "react-icons/gr";
+import FinanceSalesForm from "../farm/finance/FinanceSalesForm";
+import FinanceExpenseFom from "../farm/finance/FinanceExpenseFom";
 
 export default function BothHeader({ type }: { type: string }) {
+  const [open, setOpen] = useState(false);
   return (
     <div className="pb-5 flex gap-3 md:flex-row flex-col md:items-center justify-between">
       <div className=" space-y-1">
@@ -14,10 +21,33 @@ export default function BothHeader({ type }: { type: string }) {
         </p>
       </div>
       <div className=" flex items-center gap-2">
-        <Button className="bg-primary-green w-[48%] sm:w-fit cursor-pointer text-white rounded-sm">
+        <Button
+          onClick={() => setOpen(true)}
+          className="bg-primary-green w-[48%] sm:w-fit cursor-pointer text-white rounded-sm"
+        >
           <GoPlus />
           <p>Add {type === "sales" ? "Sale" : "Expense"}</p>
         </Button>
+
+        <FinanceModal
+          text={
+            type === "sales"
+              ? "Record a new sale made from your farm"
+              : "Record a new expense for your farm"
+          }
+          type={type === "sales" ? "Sale" : "Expense"}
+          iconColor={
+            type === "sales"
+              ? "bg-green-100 text-green-500"
+              : "bg-red-100 text-red-500"
+          }
+          Icon={type === "sales" ? FiShoppingCart : GrMoney}
+          open={open}
+          onClose={() => setOpen(false)}
+        >
+          {type === "sales" ? <FinanceSalesForm /> : <FinanceExpenseFom />}
+        </FinanceModal>
+
         <Button className="bg-transparent border border-dark/15 w-[48%] sm:w-fit cursor-pointer text-dark/90 rounded-sm">
           <FiDownload />
 
